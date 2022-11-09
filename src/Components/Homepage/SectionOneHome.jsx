7bfc11500bf6a62b454a17c057f34aae337abc49
NateLapak <Nate.lapak12@gmail.com>
2022-11-09 10:16:43 -0700

    First commit

---
+++ src/Components/Homepage/SectionOneHome.jsx
@@ -0,0 +1,12 @@
+import "./SectionOneHome.css"
+
+// First section of homepage
+const SectionOneHome = () => {
+    return (
+        <div className="SectionOneHome">
+            <h2>Section one of homepage</h2>
+        </div>
+    )
+}
+
+export default SectionOneHome
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import SectionOneHome from "../Components/Homepage/SectionOneHome"
+
+// Render homepage components into HomePage
+const Homepage = () => {
+    return (
+        <div>
+            <SectionOneHome />
+        </div>
+    )
+}
+
+export default Homepage
